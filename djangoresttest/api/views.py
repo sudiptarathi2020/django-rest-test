fac0cf25bf924b240d8ab9dd755f42a93468c69c
sudiptarathi2020 <rathisudipta99@gmail.com>
2024-12-30 13:34:22 +0600

    create blog post api

--- djangoresttest/api/views.py
+++ djangoresttest/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from api.models import BlogPost
+from api.serializers import BlogPostSerializer
+
 
 # Create your views here.
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
