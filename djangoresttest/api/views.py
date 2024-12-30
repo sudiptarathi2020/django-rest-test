b09ac0226781a088d0f96f203a9a0d36f89aa0e5
sudiptarathi2020 <rathisudipta99@gmail.com>
2024-12-30 14:17:14 +0600

    added more views

--- djangoresttest/api/views.py
+++ djangoresttest/api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from api.models import BlogPost
 from api.serializers import BlogPostSerializer
@@ -10,3 +10,12 @@ from api.serializers import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    def delete(self,request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
